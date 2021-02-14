e9b1738fe052b39e17e97f746b06438a8be8cfc5
besforyou999 <besforyou2@gmail.com>
2021-02-14 11:59:17 +0900

    first commit

---
+++ src/main/java/Menu.java
@@ -0,0 +1,46 @@
+import java.util.Vector;
+
+public class Menu {
+
+    public static final String LOCKER_TOP = "┌ ────── ┐";
+    public static final String OCCUPATION_NUBMER = "| %d   %02d |";
+    public static final String LOCKER_BOTTOM = "└ ────── ┘";
+
+    private Vector<Integer> occupation = new Vector<Integer>();
+    private Vector<Integer> fee = new Vector<Integer>();
+
+    Menu() {
+        for(int i=0; i < 50; i++) {
+            occupation.add(0);
+            fee.add(0);
+        }
+    }
+
+    public void PrintLockers() {
+        PrintLockersFrom(0,9);
+        PrintLockersFrom(10,19);
+        PrintLockersFrom(20,29);
+        PrintLockersFrom(30,39);
+        PrintLockersFrom(40,49);
+    }
+
+
+    public void PrintLockersFrom(int start, int end) {
+
+        for(int i=start; i < end; i++) {
+            System.out.print(LOCKER_TOP);
+        }
+        System.out.println();
+        for(int i=start; i < end; i++) {
+            System.out.printf(OCCUPATION_NUBMER,occupation.get(i),fee.get(i));
+        }
+        System.out.println();
+        for(int i=start; i < end; i++) {
+            System.out.print(LOCKER_BOTTOM);
+        }
+        System.out.println();
+    }
+
+
+
+}
